ec4a918704059cb3b5603750757b1028817db2db
Mads <maller2650@gmail.com>
2020-05-21 13:07:04 +0200

    Initial setup

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class HomeResource {
+public class HomeController {
 
     @GetMapping("/")
     public String index(){
---
+++ src/main/resources/sql/DML-populate-schema.sql
@@ -0,0 +1,118 @@
+USE nordic_motorhome_rental;
+
+SELECT * FROM departments;
+SELECT * FROM users;
+SELECT * FROM authorities;
+SELECT * FROM employees;
+SELECT * FROM accessories;
+SELECT * FROM autocampers;
+
+INSERT INTO departments
+(id, name)
+VALUES
+(DEFAULT, "Bestyrelse"),
+(DEFAULT, "Salg"),
+(DEFAULT, "Rengøring"),
+(DEFAULT, "Bilservice"),
+(DEFAULT, "Bogholderi");
+
+INSERT INTO users
+(username, password, enabled)
+VALUES
+("Ejer1", "ejer1", TRUE),
+("Salg1", "salg1", TRUE),
+("Salg2", "salg2", TRUE),
+("Salg3", "salg3", TRUE),
+("Rengøring1", "rengøring1", TRUE),
+("Mekaniker1", "mekaniker1", TRUE),
+("Bogholder1", "bogholder1", TRUE);
+
+INSERT INTO authorities
+(username, authority)
+VALUES
+("Ejer1", "ADMIN"),
+("Salg1", "SALG"),
+("Salg2", "SALG"),
+("Salg3", "SALG"),
+("Rengøring1", "RENGØRING"),
+("Mekaniker1", "MEKANIKER"),
+("Bogholder1", "BOGHOLDER");
+
+INSERT INTO employees
+(id, department_id, users_username, first_name, last_name, phone, mail, zip_code, city, address)
+VALUES
+(DEFAULT, (SELECT id FROM departments WHERE name = "Bestyrelse"), "Ejer1", "David", "Harry Ema", 46460400, "de@kea.dk", 2200, "København N", "Lygten 37"),
+(DEFAULT, (SELECT id FROM departments WHERE name = "Salg"), "Salg1", "Salg", "Test1", 00000000, "salg@nordicmotorhome.dk", 2200, "København N", "Lygten 37"),
+(DEFAULT, (SELECT id FROM departments WHERE name = "Salg"), "Salg2", "Salg", "Test2", 00000000, "salg@nordicmotorhome.dk", 2200, "København N", "Lygten 37"),
+(DEFAULT, (SELECT id FROM departments WHERE name = "Salg"), "Salg3", "Salg", "Test3", 00000000, "salg@nordicmotorhome.dk", 2200, "København N", "Lygten 37"),
+(DEFAULT, (SELECT id FROM departments WHERE name = "Rengøring"), "Rengøring1", "Rengøring", "Test1", 00000000, "rengøring@nordicmotorhome.dk", 2200, "København N", "Lygten 37"),
+(DEFAULT, (SELECT id FROM departments WHERE name = "Bilservice"), "Mekaniker1", "Mekaniker", "Test1", 00000000, "bilservice@nordicmotorhome.dk", 2200, "København N", "Lygten 37"),
+(DEFAULT, (SELECT id FROM departments WHERE name = "Bogholderi"), "Bogholder1", "Bogholder", "Test1", 00000000, "boholderi@nordicmotorhome.dk", 2200, "København N", "Lygten 37");
+
+INSERT INTO accessories
+(id, name, description, price)
+VALUES
+(DEFAULT, "Cykelstativ", "Dette er et cykelstativ hvor du kan have dine cykler. Der er plads til 4 cykler", 200),
+(DEFAULT, "Sengetøj", "Ekstra sengetøj i lækker kvalitet. Oplagt til gæster.", 50),
+(DEFAULT, "Barnesæde", "Til de små der gerne vil følge med i verden mens far eller mor kører bil.", 50),
+(DEFAULT, "Picnicbord", "Ønsker i at nyde frokosten i naturen eller bare on-the-go er et picnicbord oplagt.", 100),
+(DEFAULT, "Klapstol", "Nem stol der kan bruges til alle formål.", 50);
+
+INSERT INTO customers
+(id, first_name, last_name, phone, mail, zip_code, city, address)
+VALUES
+(DEFAULT, "John", "Larsen", 12345678, "larsen37@mail.dk", 2800, "Kongens Lyngby", "Kastanievej 5"),
+(DEFAULT, "John", "Smith", 12345678, "johnsmith@mail.dk", 2800, "Kongens Lyngby", "Jernbanevej 3A"),
+(DEFAULT, "Karen", "Bibo", 12345678, "mail@mail.dk", 2800, "Kongens Lyngby", "Toftebæksvej 9"),
+(DEFAULT, "Marie", "Nissen", 12345678, "missen@mail.dk", 2300, "København S", "Gladiolusvej 4"),
+(DEFAULT, "Malene", "Larsen", 12345678, "m.larsen@mail.dk", 2800, "Kongens Lyngby", "Kastanievej 2"),
+(DEFAULT, "Morten", "Ankerstjerne", 12345678, "anker@mail.dk", 2300, "København S", "Thorshavnsgade 18A"),
+(DEFAULT, "Micklas", "Nillekestøv", 12345678, "m.nille.støv@mail.dk", 2300, "København S", "Boltonvej 18"),
+(DEFAULT, "Anders", "Bæk", 12345678, "andersbaek@mail.dk", 4100, "Ringsted", "Fluebæksvej 27");
+
+INSERT INTO autocamper_types
+(brand, model, beds, seats, description)
+VALUES
+("Fiat", "Ducato 35", 2, 4, "Der er en fin siddegruppe midt i vognen, hvor 2 personer kan være fastspændt under kørsel. Siddegruppen kan redes op til sengeplads.
+Autocamperen er godkendt til 4 personer under transport. Der er en dobbeltseng og mulighed for opredning til 2 børn."),
+("Fiat", "Ducato 30", 1, 2, "Siddepladser til chauffør samt 1 ved siden af. Autocamperen er godkendt til 2 personer under transport. Der er en dobbeltseng og ikke mulighed for ekstra opredning."),
+("Fiat", "Weinsberg", 4, 4, "Ren luksus autocamper. Der er en fin siddegruppe midt i vognen, hvor 2 personer kan være fastspændt under kørsel. Siddegruppen kan redes op til sengeplads.
+Autocamperen er godkendt til 4 personer under transport. Der er en dobbeltseng og mulighed for opredning til 2 børn."),
+("Knaus", "Van TI", 2, 4, "Knaus Van TI er ok, men ikke ligeså god som Van TI Plus!"),
+("Knaus", "Van TI Plus", 4, 4, "Knaus Van TI Plus er 10 gange bedre end Van TI");
+
+INSERT INTO autocampers
+(id, autocamper_type_brand, autocamper_type_model, year, price_day)
+VALUES
+(DEFAULT, "Fiat", "Ducato 35", 2011, 2500.00),
+(DEFAULT, "Fiat", "Ducato 35", 2011, 2500.00),
+(DEFAULT, "Fiat", "Ducato 35", 2011, 2500.00),
+(DEFAULT, "Fiat", "Ducato 35", 2011, 2500.00),
+(DEFAULT, "Fiat", "Ducato 35", 2015, 3000.00),
+(DEFAULT, "Fiat", "Ducato 35", 2015, 3000.00),
+(DEFAULT, "Fiat", "Ducato 35", 2015, 3000.00),
+(DEFAULT, "Fiat", "Ducato 35", 2016, 3200.00),
+(DEFAULT, "Fiat", "Ducato 35", 2017, 3500.00),
+(DEFAULT, "Fiat", "Ducato 35", 2017, 3500.00),
+(DEFAULT, "Fiat", "Ducato 35", 2017, 3500.00),
+(DEFAULT, "Fiat", "Ducato 35", 2017, 3500.00),
+(DEFAULT, "Fiat", "Ducato 30", 2005, 1500.00),
+(DEFAULT, "Fiat", "Ducato 30", 2005, 1500.00),
+(DEFAULT, "Fiat", "Ducato 30", 2005, 1500.00),
+(DEFAULT, "Fiat", "Ducato 30", 2015, 2000.00),
+(DEFAULT, "Fiat", "Ducato 30", 2015, 2000.00),
+(DEFAULT, "Fiat", "Ducato 30", 2019, 2500.00),
+(DEFAULT, "Fiat", "Weinsberg", 2019, 4000.00),
+(DEFAULT, "Fiat", "Weinsberg", 2019, 4000.00),
+(DEFAULT, "Fiat", "Weinsberg", 2019, 4000.00),
+(DEFAULT, "Fiat", "Weinsberg", 2019, 4000.00),
+(DEFAULT, "Knaus", "Van TI", 2019, 4300.00),
+(DEFAULT, "Knaus", "Van TI", 2019, 4300.00),
+(DEFAULT, "Knaus", "Van TI", 2019, 4300.00),
+(DEFAULT, "Knaus", "Van TI", 2020, 4500.00),
+(DEFAULT, "Knaus", "Van TI", 2020, 4500.00),
+(DEFAULT, "Knaus", "Van TI", 2020, 4500.00),
+(DEFAULT, "Knaus", "Van TI", 2020, 4500.00),
+(DEFAULT, "Knaus", "Van TI Plus", 2020, 5000.00),
+(DEFAULT, "Knaus", "Van TI Plus", 2020, 5000.00),
+(DEFAULT, "Knaus", "Van TI Plus", 2020, 5000.00);
