11f8ce0db0af9585e1bb5d3aab11a68bd6c3f162
Michael Fuglø <michael@fuglo.com>
2020-05-25 16:09:17 +0200

    Added Booking de sidste bookingDAO metoder (create, read, update, delete)

--- src/main/java/com/example/demo/repository/BookingDAO.java
+++ src/main/java/com/example/demo/repository/BookingDAO.java
@@ -1,9 +1,6 @@
 package com.example.demo.repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,22 +14,88 @@ public class BookingDAO implements CRUDRepository<BookingDTO> {
 
     @Override
     public void create(BookingDTO bookingDTO) {
+        try{
+            PreparedStatement statementToInsert = conn.prepareStatement("INSERT INTO bookings (autocamper_id, customer_id, period_start, period_end, dropoff, pickup, price_total) " +
+                    " VALUES (?,?,?,?,?,?,?)");
+            statementToInsert.setInt(1,bookingDTO.getAutocamperId());
+            statementToInsert.setInt(2,bookingDTO.getCustomerId());
+            statementToInsert.setDate(3, Date.valueOf(bookingDTO.getPeriodStart()));
+            statementToInsert.setDate(4, Date.valueOf(bookingDTO.getPeriodEnd()));
+            statementToInsert.setString(5, bookingDTO.getDropOff());
+            statementToInsert.setString(6, bookingDTO.getPickUp());
+            statementToInsert.setInt(7, bookingDTO.getPriceTotal());
 
+            statementToInsert.executeUpdate();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public BookingDTO read(int id) {
-        return null;
+        BookingDTO bookingToReturn = new BookingDTO();
+
+        try{
+            PreparedStatement statementToQuery = conn.prepareStatement("SELECT * FROM bookings WHERE id=?");
+            statementToQuery.setInt(1,id);
+            ResultSet rs = statementToQuery.executeQuery();
+
+            bookingToReturn.setId(rs.getInt(1));
+            bookingToReturn.setAutocamperId(rs.getInt(2));
+            bookingToReturn.setCustomerId(rs.getInt(3));
+            bookingToReturn.setPeriodStart(rs.getDate(4).toLocalDate());
+            bookingToReturn.setPeriodEnd(rs.getDate(5).toLocalDate());
+            bookingToReturn.setDropOff(rs.getString(6));
+            bookingToReturn.setPickUp(rs.getString(7));
+            bookingToReturn.setPriceTotal(rs.getInt(8));
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return bookingToReturn;
     }
 
     @Override
     public void update(BookingDTO bookingDTO) {
+            try {
+                PreparedStatement statementToExecute = conn.prepareStatement("UPDATE bookings SET " +
+                        "autocamper_id = ?, " +
+                        "customer_id = ?, " +
+                        "period_start = ?, " +
+                        "period_end = ?, " +
+                        "dropoff = ?," +
+                        "pickup = ?," +
+                        "price_total = ?" +
+                        "WHERE id = ?");
 
+                statementToExecute.setInt(1,bookingDTO.getAutocamperId());
+                statementToExecute.setInt(2,bookingDTO.getCustomerId());
+                statementToExecute.setDate(3,Date.valueOf(bookingDTO.getPeriodStart()));
+                statementToExecute.setDate(4,Date.valueOf(bookingDTO.getPeriodEnd()));
+                statementToExecute.setString(5,bookingDTO.getDropOff());
+                statementToExecute.setString(6,bookingDTO.getPickUp());
+                statementToExecute.setInt(7,bookingDTO.getPriceTotal());
+
+                statementToExecute.executeUpdate();
+
+        }
+            catch (SQLException e){
+                e.printStackTrace();
+            }
     }
 
     @Override
     public void delete(int id) {
-
+        try{
+            PreparedStatement statementToExecute = conn.prepareStatement("DELETE FROM bookings WHERE id=?");
+            statementToExecute.setInt(1,id);
+            statementToExecute.executeUpdate();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
